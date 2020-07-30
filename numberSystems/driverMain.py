d3ab8c426f9b7df932a8c55d9967ce796d0e2f05
MBrill <manfred.brill@hs-kl.de>
2020-07-30 11:02:59 +0200

    doxygen docus und polishing

--- numberSystems/driverMain.py
+++ numberSystems/driverMain.py
@@ -1,62 +1,68 @@
 # -*- coding: utf-8 -*-
-"""
-@author: brill
-"""
-# Driver code  
-import fractions as fr
+
+
+## \example
+# Driver for place system functions
+#
+# We convert natural and rational numbers
+# from and to place systems with base 10, 2, 8
+# and 16.
+
+
+import placesystems as ps
 
 if __name__ == "__main__" : 
   
-    # Natürliche Zahlen
+    # Natural numbers
     n = 125   
     base = 2
-    sol = fr.Natural2Placesystem(n, base)
+    sol = ps.natural2placesystem(n, base)
     print("Die Darstellung von ", n, "zur Basis ", base, " ist ", sol)
     
     base = 8
-    sol = fr.Natural2Placesystem(n, base)
+    sol = ps.natural2placesystem(n, base)
     print("Die Darstellung von ", n, "zur Basis ", base, " ist ", sol)
     
     n = 6
     base = 2
-    sol = fr.Natural2Placesystem(n, base)
+    sol = ps.natural2placesystem(n, base)
     
     print("Die Darstellung von ", n, "zur Basis ", base, " ist ", sol)   
     
-    # Rationale Zahlen
+    # Rational numbers
     m = 5
     n = 4
     precision = 2
     base = 10
     print("Die Darstellung von ", m, "/", n, " zur Basis ", base, " ist " , 
-          fr.rationalToPlacesystem(m, n, base, precision)) 
+          ps.rational2placesystem(m, n, base, precision)) 
 
     m = 5
     n = 4
     precision = 2
     base = 2
     print("Die Darstellung von ", m, "/", n, " zur Basis ", base, " ist " , 
-          fr.rationalToPlacesystem(m, n, base, precision)) 
+          ps.rational2placesystem(m, n, base, precision)) 
   
     m = 4
     n = 3
     precision = 10
     base = 10
     print("Die Darstellung von ", m, "/", n, " zur Basis ", base, " ist " , 
-          fr.rationalToPlacesystem(m, n, base, precision)) 
+          ps.rational2placesystem(m, n, base, precision)) 
     
     base = 2
     print("Die Darstellung von ", m, "/", n, " zur Basis ", base, " ist " , 
-          fr.rationalToPlacesystem(m, n, base, precision)) 
+          ps.rational2placesystem(m, n, base, precision)) 
 
     m = 1
     n = 10
     precision = 5
     base = 2
     print("Die Darstellung von ", m, "/", n, " zur Basis ", base, " ist " , 
-          fr.rationalToPlacesystem(m, n, base, precision)) 
+          ps.rational2placesystem(m, n, base, precision)) 
     
     precision = 5
     base = 8
     print("Die Darstellung von ", m, "/", n, " zur Basis ", base, " ist " , 
-          fr.rationalToPlacesystem(m, n, base, precision))
+          ps.rational2placesystem(m, n, base, precision))
---
+++ numberSystems/placesystems.py
@@ -0,0 +1,68 @@
+## We convert natural and rational numbers to place systems defined by the base.
+
+## \mainpage
+# Examples and functions for place systems.
+#
+# \image html pdepackweb.png
+#
+# The input for this page can be found in the file fractions.py.
+
+import math
+
+## Convert a natural number to a place system
+#
+# \param num: natural number to be converted
+# \param base: base of the place system to be used
+#              base=2 is the default
+# \return the digits in the place system as a string
+def natural2placesystem(num, base=2) : 
+
+    result = ""    
+    # Wir bestimmen k, die Anzahl der Ziffern
+    k = math.floor(math.log(num, base)) + 1
+    divisor = base**(k-1)
+    while (k>0) :         
+        digit = num // divisor  
+        num = num % divisor 
+        # Ziffer in String speichern 
+        result += str(digit);  
+  
+        k -= 1        
+        divisor //= base
+        
+    return result
+
+## Convert a rational number to a place system
+#
+# \param m: nominator of rational number q=m/n
+# \param n: ndeominator of rational number q=m/n
+# \param base: base of the place system to be used
+#              base=2 is the default
+# @param precision: number of digits used after the point
+#                   default value is 10
+# \return the digits in the place system as a string
+def rational2placesystem(m, n, base, precision = 10) : 
+  
+    result = ""  
+  
+    # Den ganzzahligen Anteil n bestimmen 
+    g = m // n  
+    if g > 0 : 
+        result = natural2placesystem(g, base) + "." 
+    else:
+        result = "0."
+           
+    # Den Anteil der Zahl nach dem Dezimalpunkt bestimmen  
+    r0 = m - g*n
+  
+    # Hier noch meinen Algorithmus einbauen,
+    # mit allgemeiner Basis und eventuell auch
+    # ein Stop vor k_prec, wenn 
+    while (precision>0 and r0>0) :            
+        r0 *= base
+        d = r0 // n
+        result += str(d)
+        r0 -= d*n 
+        precision -= 1
+
+    return result  
