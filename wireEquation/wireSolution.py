b297f7cf7b2e30be94e41d7959a7f26166c7a2cc
MBrill <manfred.brill@hs-kl.de>
2020-09-24 14:33:57 +0200

    Tomografie und Kabelgleichung

--- wireEquation/wireSolution.py
+++ wireEquation/wireSolution.py
@@ -14,14 +14,14 @@ import matplotlib.pyplot as plt
 
 # our system and the parametersof the wire equation
 # boundary condition
-a = 0.0
-b = 0.0
+ya = -1.0
+yb = 1.0
 # constant force (don't forget the minus sign!)
-f = -0.5
+f = -10.0
 # length of our wire
 length = 1.0
 # discretization parameters
-n = 20
+n = 50
 h = length/n
 
 # set the tridiagonal matrix of our system
@@ -33,34 +33,32 @@ aa[1, :] = diag
 aa[2, :-1] = offdiag
 # right-hand side
 b = np.zeros((n, 1))
-# Hier noch etwas machen: die rechte Seite hängt von n ab!
-# sonst werden die Bilder für variables n falsch!
-# für n=10 ist das Ergebnis korrekt1
-b[:] = f
-
+b[:] = h*h*f
+# boundary values
+b[0] += ya
+b[-1] += yb
 # Call SciPy solver
 x = solve_banded((1, 1), aa, b)
 
 # Vis
 # We add the boundary points using a and b for the y-values
 # and Matplotlib.
-wireX = np.linspace(0.0, n+1, n+2)
-wireY = np.zeros((n+2, 1))
-wireY[1:n+1] = x[0:n]
+wireX = np.linspace(0.0, length, n+2)
+wireY = np.linspace(0.0, length, n+2)
+
+wireY[0] = ya
+wireY[1: -2] = x[:-1, 0]
+wireY[n] = x[-1, 0]
+wireY[n+1] = yb
 
 fig = plt.figure(figsize=(16.0, 9.0))
 plt.figure(dpi=600)
 plt.grid(True)
-plt.axis([0.0, np.ceil(wireX[n+1]), np.floor(x.min()), 0.5])
-plt.tick_params(
-    axis='x',          # changes apply to the x-axis
-    which='both',      # both major and minor ticks are affected
-    bottom=False,      # ticks along the bottom edge are off
-    top=False,         # ticks along the top edge are off
-    labelbottom=False) # labels along the bottom edge are off
+plt.axis([0.0, length, np.floor(wireY.min()), max(ya, yb)+1.0])
+plt.xticks([])
+# plt.yticks([])
 plt.plot(wireX, wireY, 'g-')
-
-plt.title('Kabelgleichung für a=b=0')
+plt.title('Lösung der Kabelgleichung')
 
 # plt.savefig('images/lambdaExample.png')
 plt.show()
