85c68bb0b93dea2233e3cdb27cdfe218255cb6ee
MBrill <manfred.brill@hs-kl.de>
2020-12-12 14:18:51 +0100

    2D Nullstellen

---
+++ NonlinearEquations/scpRoot2D.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Beispiele für die numerische Nullstellensuche für eine Menge
+von nicht-linearen Gleichungen mit SciPy
+"""
+from scipy import optimize
+import functions as ft
+
+
+print('Numerische Nullstellensuche mit dem Brent-Algorithmus')
+print('Ausgaben')
+print('Nullstelle, Anzahl der Iterationen, Anzahl der Funktionsaufrufe')
+print('')
+
+
+sol = optimize.root_scalar(ft.f,
+                           bracket=[0, 3],
+                           method='brentq')
+if sol.converged:
+    print('Brent-Algorithmus')
+    print(sol.root, sol.iterations, sol.function_calls)
+else:
+    print('Fehler im Brent-Algorithmus')
