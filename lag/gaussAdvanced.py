88805cf00791136400ec3ecaa8c56cc62b43f811
MBrill <manfred.brill@hskl.de>
2022-07-20 10:20:19 +0200

    3x3 Fall ist jetzt beim Ray-Tracer implementiert
    On branch develop
    Your branch is up to date with 'origin/develop'.
    
    Changes to be committed:
    modified:   lag/gaussAdvanced.py

--- lag/gaussAdvanced.py
+++ lag/gaussAdvanced.py
@@ -35,14 +35,6 @@ def backSubstitution(a: np.float64):
     return x
 
 
-def cool3(a):
-    # n=3
-    # Schritt 1
-    a[1, 1:] = a[1, 1:] - (a[1, 0]/a[0, 0]) * a[0, 1:]
-    a[2, 1:] = a[2, 1:] - (a[2, 0]/a[0, 0]) * a[0, 1:]
-    # Schritt 2
-    a[2, 2:] = a[2, 2:] - (a[2, 1]/a[1, 1]) * a[1, 2:]
-
 
 def gauss(a):
     """
