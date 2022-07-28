98f806bb3153ae975a0c9f9c292de1ed0484bc26
MBrill <manfred.brill@hskl.de>
2022-07-28 13:17:32 +0200

    Bandmatrizen, LU und Hilbertstudie überarbeitet
    On branch develop
    Your branch is up to date with 'origin/develop'.
    
    Changes to be committed:
    modified:   lag/bandedSolve.py
    modified:   lag/choleskySolve.py
    modified:   lag/hilbertmatrix.py
    new file:   lag/hilbertmatrixLGS.py
    modified:   lag/scipyLUSolve.py

---
+++ lag/hilbertmatrixLGS.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Fallstudie: Invertierung der Hilbert-Matrix
+"""
+
+import numpy as np
+from scipy import linalg
+
+
+
+
+
+
+# We solve a linear equation system for the columns of Hinv
+# Use the identiy matrix as right hand side
+rhs = np.identity(n)
+
+computed_inv = np.linalg.solve(hilbert, rhs)
+print('Result of the computation of the inverse using linalg.solve')
+print(computed_inv)
+
+print('Relative Error for computing the inverse of a Hilbert matrix')
+print('n = ', n)
+# relativer Fehler noch implementieren!
+print(np.linalg.norm(hilbert_inv - computed_inv))
