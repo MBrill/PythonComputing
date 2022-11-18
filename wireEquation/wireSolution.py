9e0deb30abf888a983bfff61c23a36d2415dead0
MBrill <manfred.brill@hskl.de>
2022-11-18 14:42:44 +0100

    Kabelgleichung und Blur
    On branch develop
    Your branch is up to date with 'origin/develop'.
    
    Changes to be committed:
    new file:   blurCase/bandedSolve.py
    new file:   blurCase/smallExample.py
    modified:   wireEquation/wireSolution.py

--- wireEquation/wireSolution.py
+++ wireEquation/wireSolution.py
@@ -1,48 +1,46 @@
 # -*- coding: utf-8 -*-
 """
-Solving the wire equation using NumPy and SciPy.
-
-We could use our own tridiagonal LU solver,
-or just solve from numpy.linalg.
-
-Here we use the solve_banded from scipy.
-Note we do not use the symmetry of the matrix.
+Lösung der diskretisierten Kabelgleichung mit
+Bandmatrizen in SciPy.
 """
 import numpy as np
 from scipy.linalg import solve_banded
 import matplotlib.pyplot as plt
 
-# our system and the parametersof the wire equation
+# our system and the parameters of the wire equation
 # boundary condition
 ya = -1.0
-yb = 1.0
+yb = 2.0
 # constant force (don't forget the minus sign!)
-f = -10.0
+f = -20.0
 # length of our wire
 length = 1.0
 # discretization parameters
 n = 50
 h = length/n
 
-# set the tridiagonal matrix of our system
+# Tridiagolmatrix als SciPy Bandmatrix
+lowerDiag = 1
+upperDiag = 1
 diag = 2.0
 offdiag = -1.0
 aa = np.zeros((3, n))
 aa[0, 1:] = offdiag
 aa[1, :] = diag
 aa[2, :-1] = offdiag
-# right-hand side
+# rechte Seite
 b = np.zeros((n, 1))
 b[:] = h*h*f
-# boundary values
+# Randbedingungen in die rechte Seite einpflegen
 b[0] += ya
 b[-1] += yb
-# Call SciPy solver
+# SciPy aufrufen. Die Bandmatrix
+# ist durch (lowerDiag, upperDiag), aa gegeben.
 x = solve_banded((1, 1), aa, b)
 
 # Vis
-# We add the boundary points using a and b for the y-values
-# and Matplotlib.
+# Wir fügen die Randpunkten mit Hilfe von a und b hinzu.
+# und verwenden Matplotlib.
 wireX = np.linspace(0.0, length, n+2)
 wireY = np.linspace(0.0, length, n+2)
 
@@ -60,5 +58,5 @@ plt.xticks([])
 plt.plot(wireX, wireY, 'g-')
 plt.title('Lösung der Kabelgleichung')
 
-# plt.savefig('images/lambdaExample.png')
+# plt.savefig('images/pdf2.png')
 plt.show()
